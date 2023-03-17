872ce1920f8bce2b54bf6f307c01cb20c33bd241
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-03-17 15:06:37 -0700

    Fix operators in 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	m = n % 10;
 	if (m > 5)
 		printf("Last digit of %d is %d and is greater than 5", n, m);
-	else if (m < 6 && m != 0)
+	else if (m == 0)
 		printf("Last digit of %d is %d and is 0", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
