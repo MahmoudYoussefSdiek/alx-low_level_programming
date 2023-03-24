846ef50a564e5df4c4b5c35c41099e2d98d01c38
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-03-23 19:56:05 -0700

    Create 8-print_square.c file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - prints a straight line in the terminal
+ * @size: number of times the character _ should be printed
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (column = 1; column <= size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
