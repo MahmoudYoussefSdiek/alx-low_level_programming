b357390c114af8acb713b3f0d523948d911b9acf
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-03-23 19:45:18 -0700

    Fix 5-more_numbers.c code logic

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * more_numbers - function that prints 10 times the numbers, from 0 to 14
- *
  * Return:  0
  */
 
@@ -14,7 +13,9 @@ void more_numbers(void)
 	{
 		for (n = 0 ; n <= 14 ; n++)
 		{
-			_putchar(n + '0');
+			if (n > 9)
+				_putchar(1 + '0');
+			_putchar((n % 10) + '0');
 		}
 		 _putchar('\n');
 	}
