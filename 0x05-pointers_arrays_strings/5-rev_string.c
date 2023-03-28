36e1ac090fd7c8925e808893a2c3cfee58f305f1
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-03-27 18:09:25 -0700

    Fix logic in 5-rev_string.c file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * rev_string - function that reverses a string
@@ -10,7 +11,7 @@
 
 void rev_string(char *s)
 {
-	int length = _strlen(s);
+	int length = strlen(s);
 	int i;
 	int j;
 	char flag;
