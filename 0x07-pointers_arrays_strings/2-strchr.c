d18ca397583318f0ca382fbf7b1a2585baf21e4f
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-03 15:47:25 -0700

    Create 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @c: character we will use it to search
+ * @s:  string we will search in
+ *
+ * Return: pointer s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int length = strlen(s);
+
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
