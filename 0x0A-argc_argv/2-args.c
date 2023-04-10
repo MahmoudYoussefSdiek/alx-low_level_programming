bb64a2b463f9968ce56d1856e3bff3669af3134c
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-10 08:25:20 -0700

    Create 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -  program that prints all arguments it receives.
+ *
+ * @argc: number of argument
+ * @argv: array of strings (argument)
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
