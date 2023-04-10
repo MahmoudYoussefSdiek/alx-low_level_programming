d00afa326c0efc460301680530a4471f3173d248
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-10 08:08:02 -0700

    Create 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -  program that prints the number of arguments passed into it.
+ *
+ * @argc: number of argument
+ * @argv: array of strings (argument)
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
