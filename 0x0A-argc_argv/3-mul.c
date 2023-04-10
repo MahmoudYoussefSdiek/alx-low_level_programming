b18f5619f8566025e8a21c2549e2706cce2966a7
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-10 08:50:01 -0700

    Fix code in 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,8 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
-		return ("%d\n", sum);
+		printf("%d\n", sum);
+		return (0);
 	}
 
 	printf("Error");
