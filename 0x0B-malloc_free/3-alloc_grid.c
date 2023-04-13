8eb9f6f38da8442cd7da1712ffcb27616efd85df
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-12 18:55:01 -0700

    Fix code in 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,19 +20,26 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = calloc(sizeof(int *), height);
+	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = calloc(sizeof(int), width);
+		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 				free(array[j]);
 			free(array);
 			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
 		}
 	}
 
