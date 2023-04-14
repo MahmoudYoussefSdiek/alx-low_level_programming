30c4bb70c3ff11dc58ea1f5291331197955436fe
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-04-14 04:40:49 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (i = 0; i < old_size && i < new_size; i++)
-		(char *)new_p[i] = (char *)ptr[i];
+		*((char *)new_p + i) = *((char *)ptr + i);
 	free(ptr);
 	return (new_p);
 }
