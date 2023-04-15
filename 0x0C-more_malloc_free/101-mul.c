fc9fe71c67d23d6e513b23d1c7f0e3efc96df584
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-14 19:28:45 -0700

    Fix code in 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -58,35 +58,34 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *string_1, *string_2;
-	int length_1, length_2, memory_size, i,
-	    over_value, digit1, digit2, *result, flag = 0;
+	int length1, length2, memory_s, i, over_v, digit1, digit2, *result, flag = 0;
 
 	string_1 = argv[1], string_2 = argv[2];
 	if (argc != 3 || !is_digit(string_1) || !is_digit(string_2))
 		errors();
-	length_1 = _strlen(string_1);
-	length_2 = _strlen(string_2);
-	memory_size = length_1 + length_2 + 1;
-	result = malloc(sizeof(int) * memory_size);
+	length1 = _strlen(string_1);
+	length2 = _strlen(string_2);
+	memory_s = length1 + length2 + 1;
+	result = malloc(sizeof(int) * memory_s);
 	if (result == NULL)
 		return (1);
-	for (i = 0; i <= length_1 + length_2; i++)
+	for (i = 0; i <= length1 + length2; i++)
 		result[i] = 0;
-	for (length_1 = length_1 - 1; length_1 >= 0; length_1--)
+	for (length1 = length1 - 1; length1 >= 0; length1--)
 	{
-		digit1 = string_1[length_1] - '0';
-		over_value = 0;
-		for (length_2 = length_2 - 1; length_2 >= 0; length_2--)
+		digit1 = string_1[length1] - '0';
+		over_v = 0;
+		for (length2 = length2 - 1; length2 >= 0; length2--)
 		{
-			digit2 = string_2[length_2] - '0';
-			over_value += result[length_1 + length_2 + 1] + (digit1 * digit2);
-			result[length_1 + length_2 + 1] = over_value % 10;
-			over_value /= 10;
+			digit2 = string_2[length2] - '0';
+			over_v += result[length1 + length2 + 1] + (digit1 * digit2);
+			result[length1 + length2 + 1] = over_v % 10;
+			over_v /= 10;
 		}
-		if (over_value > 0)
-			result[length_1 + length_2 + 1] += over_value;
+		if (over_v > 0)
+			result[length1 + length2 + 1] += over_v;
 	}
-	for (i = 0; i < memory_size - 1; i++)
+	for (i = 0; i < memory_s - 1; i++)
 	{
 		if (result[i] != 0)
 			flag = 1;
