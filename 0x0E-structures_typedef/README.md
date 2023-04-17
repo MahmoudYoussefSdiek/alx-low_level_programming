bed07712a5eec7ab54d46a027cf8c8060058b54c
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-17 15:23:37 -0700

    Create dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# structures typedef
+### Here we will make all 0x0E-structures_typedef task files in C programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that have information about dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: This is struct that have dog information
+ * (dog name, dog age, owner name)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
