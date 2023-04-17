f64de38cfba4c5af2d9764a878300afc895c03e1
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-17 15:51:51 -0700

    Remove main.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include "main.h"
 
 /**
  * struct dog - struct that have information about dog
@@ -17,5 +16,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
