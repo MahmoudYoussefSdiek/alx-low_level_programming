f489f8ea3c7a41ae96592123f91b74c5666a871e
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-20 11:53:37 -0700

    Update code in 3-print_all.c file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,53 +7,42 @@
  *
  * Return: void
  */
-/**
- * print_all - function that prints anything.
- *
- * @format: list of types of arguments passed to the function
- *
- * Return: void
- */
-
 void print_all(const char *const format, ...)
 {
-	int index = 0, flag;
+	int index, flag, length;
 	char *string;
-	char c;
 	va_list ap;
 
+	length = strlen(format);
 	va_start(ap, format);
-	while (format != NULL && index < strlen(format))
+	index = 0;
+	while (format != NULL && index < length)
 	{
-		c = format[index];
-		if (c != 'c' || c != 'i' || c != 'f' || c != 's')
-			index++;
-		else
+		flag = 0;
+		switch (format[index])
 		{
-			switch (format[index])
-			{
-				case 'c':
-					printf("%c", va_arg(ap, int));
-					break;
-				case 'i':
-					printf("%i", va_arg(ap, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(ap, double));
-					break;
-				case 's':
-					string = va_arg(ap, char *);
-					if (string == NULL)
-						string = "(nil)";
-					printf("%s", string);
-					break;
-				default:
-					break;
-			}
-			if (index < (strlen(format) - 1))
-				printf(", ");
-			index++;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				string = va_arg(ap, char *);
+				if (string == NULL)
+					string = "(nil)";
+				printf("%s", string);
+				break;
+			default:
+				flag = 1;
+				break;
 		}
+		if (index < length - 1 && flag == 0)
+			printf(", ");
+		index++;
 	}
 	printf("\n");
 	va_end(ap);
