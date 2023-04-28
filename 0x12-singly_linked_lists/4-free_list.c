7e331c706585a56186f20d678c1844f2739708f3
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-27 17:42:59 -0700

    Fix code in 4-free_list.c file

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,10 +9,12 @@
  */
 void free_list(list_t *head)
 {
-	if (head != NULL && head->next != NULL)
+	if (head != NULL)
+	{
 		free_list(head->next);
 
-	if (head->str)
-		free(head->str);
-	free(head);
+		if (head->str != NULL)
+			free(head->str);
+		free(head);
+	}
 }
