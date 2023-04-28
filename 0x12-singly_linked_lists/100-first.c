40fe8f7f8486148edeeea611d193ed44fbdf5936
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-04-27 18:05:51 -0700

    Create 100-first.c and update list.h files

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_before_main - function that prints before
+ * the main function is executed.
+ *
+ * Return: void
+ **/
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
