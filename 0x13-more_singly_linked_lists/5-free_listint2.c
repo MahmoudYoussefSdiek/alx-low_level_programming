6952d0935e715f00226e38d1fc2d1254df363381
Mahmoud Youssef <111357017+MahmoudYoussefSdiek@users.noreply.github.com>
2023-05-03 01:48:06 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,12 @@
  */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL)
+		return;
 	if (*head != NULL)
 	{
 		free_listint2(&(*head)->next);
 		free(*head);
-		*head = NULL;
 	}
+	*head = NULL;
 }
