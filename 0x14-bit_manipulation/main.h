dd585a02548fd1a4bbedd74b7220e71b971f6bbe
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-05-04 17:47:41 -0700

    Update code in main.h file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio>
-#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
