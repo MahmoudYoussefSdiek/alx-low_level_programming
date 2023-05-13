3bd5faf558a85fea1d6ced22edd6a3bcd868d107
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-05-12 17:33:35 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,11 @@
 #include <unistd.h>
 #include <string.h>
 
+#define BUFF_SIZE 1024
+
+char *create_buffer(char *file);
+void close_file(int file_descriptor);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
