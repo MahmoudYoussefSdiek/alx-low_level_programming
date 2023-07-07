5ae69e508bc44c26f003288deca43f90324240f0
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-07 11:43:01 -0700

    Update hash_tables.h file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -90,6 +90,10 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-
+void update_value(shash_node_t *node, const char *value);
+shash_node_t *create_node(const char *key, const char *value);
+void insert_node(shash_table_t *ht, shash_node_t *new_node);
+void insert_at_head(shash_table_t *ht, shash_node_t *new_node);
+void insert_in_sorted_order(shash_table_t *ht, shash_node_t *new_node);
 
 #endif
