61270e5d1cfc2988abd4e3a3212ec9a51231568c
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-07 16:29:29 -0700

    Update 100-sorted_hash_table.c and hash_table.h files

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -44,8 +44,8 @@ shash_table_t *shash_table_create(unsigned long int size)
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-	shash_node_t *node, *new_node, *temp;
 	unsigned long int index;
+	shash_node_t *node, *new_node;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
@@ -57,48 +57,127 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
-			free(node->value);
-			node->value = strdup(value);
+			update_value(node, value);
 			return (1);
 		}
 		node = node->next;
 	}
 
-	new_node = malloc(sizeof(shash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
 
+	insert_node(ht, new_node);
+	return (1);
+}
+
+/**
+ * update_value - function that updates the value
+ *
+ * @node: The node
+ * @value: The value
+ *
+ * Return: void
+ */
+void update_value(shash_node_t *node, const char *value)
+{
+	free(node->value);
+	node->value = strdup(value);
+}
+
+/**
+ * create_node - function that creates a node
+ *
+ * @key: The key
+ * @value: The value
+ *
+ * Return: The node
+ */
+shash_node_t *create_node(const char *key, const char *value)
+{
+	shash_node_t *new_node;
+
+	new_node = malloc(sizeof(shash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	new_node->next = NULL;
+	new_node->sprev = NULL;
+	new_node->snext = NULL;
+
+	return (new_node);
+}
+
+/**
+ * insert_node - function that inserts an element in a hash table
+ *
+ * @ht: The hash table
+ * @new_node: The node
+ *
+ * Return: void
+ */
+void insert_node(shash_table_t *ht, shash_node_t *new_node)
+{
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)new_node->key, ht->size);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	if (ht->shead == NULL || strcmp(key, ht->shead->key) < 0)
+	if (ht->shead == NULL || strcmp(new_node->key, ht->shead->key) < 0)
 	{
-		new_node->sprev = NULL;
-		new_node->snext = ht->shead;
-		if (ht->shead != NULL)
-			ht->shead->sprev = new_node;
-		else
-			ht->stail = new_node;
-		ht->shead = new_node;
+		insert_at_head(ht, new_node);
 	}
 	else
 	{
-		temp = ht->shead;
-		while (temp->snext != NULL && strcmp(key, temp->snext->key) > 0)
-			temp = temp->snext;
-		new_node->sprev = temp;
-		new_node->snext = temp->snext;
-		if (temp->snext != NULL)
-			temp->snext->sprev = new_node;
-		else
-			ht->stail = new_node;
-		temp->snext = new_node;
+		insert_in_sorted_order(ht, new_node);
 	}
-
-	return (1);
 }
+
+/**
+ * insert_at_head - function that inserts at head
+ *
+ * @ht: The hash table
+ * @new_node: The node
+ *
+ * Return: void
+ */
+void insert_at_head(shash_table_t *ht, shash_node_t *new_node)
+{
+	new_node->sprev = NULL;
+	new_node->snext = ht->shead;
+	if (ht->shead != NULL)
+		ht->shead->sprev = new_node;
+	else
+		ht->stail = new_node;
+	ht->shead = new_node;
+}
+
+/**
+ * insert_in_sorted_order - function that inserts an element in order
+ *
+ * @ht: The hash table
+ * @new_node: The node
+ *
+ * Return: void
+ */
+void insert_in_sorted_order(shash_table_t *ht, shash_node_t *new_node)
+{
+	shash_node_t *temp = ht->shead;
+
+	while (temp->snext != NULL && strcmp(new_node->key, temp->snext->key) > 0)
+		temp = temp->snext;
+	new_node->sprev = temp;
+	new_node->snext = temp->snext;
+	if (temp->snext != NULL)
+		temp->snext->sprev = new_node;
+	else
+		ht->stail = new_node;
+	temp->snext = new_node;
+}
+
 
 /**
  * shash_table_get - function that gets an element in a hash table
