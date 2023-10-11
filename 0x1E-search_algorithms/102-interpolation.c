04421cf1f69e2d9a7a6016aae4cd7f1db441235f
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-11 15:33:21 -0700

    Fix code in 102-interpolation.c file

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,12 +12,12 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
+	size_t low, high, pos;
+	low = 0;
+	high = size - 1;
+
 	if (array == NULL)
 		return (-1);
-
-	size_t low = 0;
-	size_t high = size - 1;
-	size_t pos;
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
