38325cb2ef4d4af4a5ff1753ab90eba727dd6129
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-11 15:47:07 -0700

    Create 103-exponential.c file

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - Searches for a value in a sorted array using
+ * the Exponential search algorithm
+ *
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index of the first occurrence of value, or -1 if not found
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1;
+	size_t left, right, mid;
+
+	if (array == NULL)
+		return (-1);
+
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	left = bound / 2;
+	right = (bound < size) ? bound : size - 1;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+
+		printf("Searching in array: ");
+		for (size_t i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
