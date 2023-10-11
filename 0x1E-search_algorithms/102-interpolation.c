3a3eb992cbab9a42a17311303ed872fd4e8317fa
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-10-11 15:22:17 -0700

    Create 102-interpolation.c file

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Searches for a value in a sorted array using
+ * the Interpolation search algorithm
+ *
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index of the first occurrence of value, or -1 if not found
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t pos;
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
+
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+
+	printf("Value checked array[%lu] is out of range\n", low);
+
+	return (-1);
+}
